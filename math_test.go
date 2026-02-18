9f3e58166f01d6ee0a7573ce18ef1ba5a00ffb5f
Victor Silva <victorsilva.silva6@gmail.com>
2026-02-18 01:15:12 -0300

    feat: quebra o teste de proposito

--- math_test.go
+++ math_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestSoma(t *testing.T) {
 
-	resultado := Soma(5, 10)
+	resultado := Soma(4, 10)
 
 	if resultado != 15 {
 		t.Errorf("Resultado = %d; esperado 15", resultado)
